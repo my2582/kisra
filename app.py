204cefca2e468fe3b9b4db3f084062c0fdb8c97f
염민수/플랫폼엔진부/한국투자증권/인터넷 <112326@truefriend-pc.ad>
2021-03-15 09:37:27 +0900

    Always run app.Dash()

--- app.py
+++ app.py
@@ -250,8 +250,7 @@ class App:
 
 
 
-if __name__ == '__main__':
-    app = App()
-    server = app.server
-    app.show_content()
-    app.app.run_server(debug = True)
+app = App()
+server = app.server
+app.show_content()
+app.app.run_server(debug = True)
