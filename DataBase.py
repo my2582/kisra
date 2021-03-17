3869c482cc9cd7e47d288158ab7d4b9a97c38a5e
changsoooo <77427114+changsoooo@users.noreply.github.com>
2021-03-17 17:54:37 +0900

    Update DataBase.py

--- DataBase.py
+++ DataBase.py
@@ -9,10 +9,12 @@ class databaseDF:
 
     def createDefault(self, data):
         try:
-            self.con.execute("CREATE TABLE general(date timestamp, userid varchar(255), asset_class varchar(255), value float(24), wt float(24))")
-            self.con.execute("CREATE TABLE detail(date timestamp, userid varchar(255), name varchar(255), asset_class varchar(255), "
+            self.con.execute("CREATE TABLE general(date varchar(255), userid varchar(255), asset_class varchar(255), value float(24), wt float(24))")
+            self.con.execute("CREATE TABLE detail(date varchar(255), userid varchar(255), name varchar(255), asset_class varchar(255), "
                                       "itemname varchar(255), quantity float(24), cost_price float(24), cost_value float(24), price float(24), value float(24), "
                                       "wt float(24), group_by varchar(255), original varchar(5))")
+            self.con.execute("CREATE TABLE USER(userid varchar(255), set_no float(24), q_no float(24), answer float(24), risk_pref_value float(24))")
+
             self.insert(data)
             self.conn.commit()
 
@@ -24,21 +26,29 @@ class databaseDF:
     def getSchema(self, data):
         general = self.selectDefault(data[0], "select * from {}")
         detail = self.selectDefault(data[1], "select * from {}")
+        user = self.selectDefault(data[2], "select * from {}")
         self.conn.commit()
-        return general, detail
+        return general, detail, user
 
     def selectDefault(self, data, query):
         return self.con.fetchall(self.con.execute(query.format(data)))
 
     def insertDefault(self, data):
-        general, detail = data
+        general, detail, user = data
         insert_query_gen = 'INSERT INTO {} (date, userid, asset_class, value, wt) values (%s, %s, %s, %s, %s)'
         insert_query_dtl = 'INSERT INTO {} (date, userid, name, asset_class, itemcode, itemname,' \
                            'quantity, cost_price, cost_value, price, value, wt, group_by, original) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
 
+        insert_query_user = 'INSERT INTO {} (userid, set_no, q_no, answer, risk_pref_value) values (%s, %s, %s, %s, %s)'
+
         detail['quantity'] = detail['quantity'].values.astype(float)
         detail['cost_price'] = detail['cost_price'].values.astype(float)
         detail['cost_value'] = detail['cost_value'].values.astype(float)
+
+        user['set_no'] = user['set_no'].values.astype(float)
+        user['q_no'] = user['q_no'].values.astype(float)
+        user['answer'] = user['answer'].values.astype(float)
+        user['risk_pref_value'] = user['risk_pref_value'].values.astype(float)
 
         for i in range(len(general)):
             temp = general.iloc[i, :].values.tolist()
@@ -47,3 +57,7 @@ class databaseDF:
         for i in range(len(detail)):
             temp = general.iloc[i, :].values.tolist()
             self.cur.execute(insert_query_dtl.format('detail'), temp)
+
+        for i in range(len(general)):
+            temp = user.iloc[i, :].values.tolist()
+            self.cur.execute(insert_query_user.format('user'), temp)
