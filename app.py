c1fb15b54351bb3e03345b0ae34824a5d6dd2091
changsoooo <77427114+changsoooo@users.noreply.github.com>
2021-03-19 13:39:00 +0900

    for heroku db

--- app.py
+++ app.py
@@ -113,7 +113,8 @@ def show_content(users):
         values = []
         idx = 0
         for i in range(1, len(for_selected.children), 2):
-            values.append(for_selected.children[i][int(outputs[idx][0])]['value'])
+            values.append(for_selected.children[i].options[int(outputs[idx][0])]['value'])
+            idx+=1
         print('-------------values-------------')
         print(values)
         return values
