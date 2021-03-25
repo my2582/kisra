3cc93f009e1be49d47a97d41917a4429f2fda131
changsoooo <77427114+changsoooo@users.noreply.github.com>
2021-03-25 12:54:22 +0900

    for img check

--- DBquery.py
+++ DBquery.py
@@ -82,7 +82,7 @@ class query:
 
     def getUserBalance(self, userid):
         query = "select distinct * from detail A " \
-                "where to_timestamp(A.date, 'mm/dd/yyyy HH:M1:SS AM') = (select max(to_timestamp(B.date, 'mm/dd/yyyy HH:M1:SS AM')) from detail B group by userid " \
+                "where to_timestamp(A.date, 'mm/dd/yyyy HH:M1:SS AM') = (select max(to_timestamp(B.date, 'mm/dd/yyyy HH:M1:SS AM')) from detail B group by B.userid " \
                 "having B.userid=%s) and A.userid=%s"
         self.con.execute(query, [userid, userid])
         self.conn.commit()
