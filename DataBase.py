d6587bceaab978887c504e31b68e641707025dcc
염민수/플랫폼엔진부/한국투자증권/인터넷 <112326@truefriend-pc.ad>
2021-05-04 13:05:25 +0900

    type

--- DataBase.py
+++ DataBase.py
@@ -22,7 +22,7 @@ class databaseDF:
         self.conn.commit()
         self.con.execute("CREATE TABLE IF NOT EXISTS userselection(userid varchar(255), name varchar(255), set_no float(24), q_no float(24), answer float(24), risk_pref_value float(24))")        
         self.conn.commit()
-        self.con.execute("CREATE TABLE IF NOT EXISTS investors(userid varchar(255), name varchar(255), acc_no varchar(20), profile_code float(4), principal flat(24))")
+        self.con.execute("CREATE TABLE IF NOT EXISTS investors(userid varchar(255), name varchar(255), acc_no varchar(20), profile_code float(4), principal float(24))")
         self.conn.commit()
 
         self.con.execute("SELECT COUNT(*) FROM detail")
