7518d7ed9677c9e3f5c477243829cbe0c5cf5605
changsoooo <dlckdtn621@gmail.com>
2021-08-03 09:37:00 +0900

    maybe final codes?

--- DASH/DASH/DashBoard/main.py
+++
@@ -1,6 +0,0 @@
-import App
-
-if __name__ == '__main__':
-    app = App.App()
-    app.show_content()
-    app.app.run_server(debug = True)
--- main.py
+++
@@ -1,7 +0,0 @@
-from app import App
-
-if __name__ == '__main__':
-    app = App()
-    server = app.server
-    app.show_content()
-    app.app.run_server(debug = True)
